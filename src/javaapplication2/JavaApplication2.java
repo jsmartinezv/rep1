ea753c846cf1daa9dbb3e11e731c2ff7db0afe8a
Usuario10 <Usuario10@FI4531119-10.unal.loc>
2016-02-09 03:39:05 -0500

    new commit22

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo 2");
         // TODO code application logic here
     }
     
